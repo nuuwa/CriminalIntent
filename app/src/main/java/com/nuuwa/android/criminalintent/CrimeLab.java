aabba6b1395dad81790122179cbbe055ba08ba8e
nuuwa <admin@nuuwa.com>
2018-04-29 23:49:45 +0800

    Add SQLite

--- app/src/main/java/com/nuuwa/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/nuuwa/android/criminalintent/CrimeLab.java
@@ -1,6 +1,7 @@
 package com.nuuwa.android.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +9,10 @@ import java.util.UUID;
 
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -18,6 +22,9 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
         mCrimes = new ArrayList<>();
     }
 
